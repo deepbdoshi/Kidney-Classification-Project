b0b2d6deab9eb28618c1a7da2ad9266ffc63f790
SUPREME-CODER <deepbdoshi@gmail.com>
2024-07-31 18:40:51 +0530

    Adding Stage to Main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kidney_disease_classifier import logger
 # from kidney_disease_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from kidney_disease_classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from kidney_disease_classifier.pipeline.stage_03_model_training import ModelTrainingPipeline
+from kidney_disease_classifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 # 1st Stage
 # STAGE_NAME = "Data Ingestion Stage"
@@ -18,16 +19,16 @@ from kidney_disease_classifier.pipeline.stage_03_model_training import ModelTrai
 
 
 # 2nd Stage
-STAGE_NAME = "Base Model Prep Stage"
+# STAGE_NAME = "Base Model Prep Stage"
 
-try:
-    logger.info(f"####### {STAGE_NAME} has started #######")
-    objPBMTP = PrepareBaseModelTrainingPipeline()
-    objPBMTP.main()
-    logger.info(f"####### {STAGE_NAME} is completed #######\n\n\n")
-except Exception as e:
-    logger.exception(e)
-    raise e
+# try:
+#     logger.info(f"####### {STAGE_NAME} has started #######")
+#     objPBMTP = PrepareBaseModelTrainingPipeline()
+#     objPBMTP.main()
+#     logger.info(f"####### {STAGE_NAME} is completed #######\n\n\n")
+# except Exception as e:
+#     logger.exception(e)
+#     raise e
 
 
 # 3rd Stage
@@ -42,3 +43,14 @@ except Exception as e:
 #     logger.exception(e)
 #     raise e
 
+# 4th Stage
+STAGE_NAME = "Model Evaluation Stage"
+try:
+    logger.info(f"####### {STAGE_NAME} has started #######")
+    objEP = EvaluationPipeline()
+    objEP.main()
+    logger.info(f"####### {STAGE_NAME} is completed #######\n\n\n")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
